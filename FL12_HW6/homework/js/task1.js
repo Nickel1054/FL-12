fe3638998632d72e323ab961790f477d7d5e4221
Yurii Pekarskyi <51463488+Nickel1054@users.noreply.github.com>
2019-12-24 19:22:40 +0200

    Add files via upload

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -2,44 +2,34 @@ let check = 1;
 let four = 4;
 let two = 2;
 
-while(check === 1){
-    var a = prompt('Enter a: ', 0);
+let a = prompt('Enter a: ', '0');
 
-    if (a === 0 || isNaN(a)){
-        console.log('Invalid input data');
-    } else {
-        break; 
-    }
+if (a === '0' || isNaN(a)){
+    console.log('Invalid input data');
 }
 
-while(check === 1){
-    var b = prompt('Enter b: ', 0);
+let b = prompt('Enter b: ', '0');
 
-    if (isNaN(b)){
-        console.log('Invalid input data');
-    } else {
-        break; 
-    }
-}
-while(check === 1){
-    var c = prompt('Enter c: ', 0);
-
-    if (isNaN(c)){
-        console.log('Invalid input data');
-    } else {
-        break; 
-    }
+if (isNaN(b)){
+    console.log('Invalid input data');
 }
 
-var discriminant = b*b - four*a*c;
+let c = prompt('Enter c: ', '0');
+
+if (isNaN(c)){
+    console.log('Invalid input data');
+}
+
+
+let discriminant = b*b - four*a*c;
 
 if (discriminant < 0){
     console.log('No possible answers (D < 0)');
 }else if (discriminant === 0){
-    var x = -b/(two*a);
+    let x = -b/(two*a);
     console.log('x = ' + Math.round(x));
 }else{
-    var x1 = (-b - Math.sqrt(discriminant))/(two*a);
-    var x2 = (-b + Math.sqrt(discriminant))/(two*a);
+    let x1 = (-b - Math.sqrt(discriminant))/(two*a);
+    let x2 = (-b + Math.sqrt(discriminant))/(two*a);
     console.log('x1 = ' + Math.round(x1) + ' and x2 = ' + Math.round(x2));
 }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,43 +1,28 @@
-let check = 1;
+let a = prompt('Enter first side length of a triangle: ', 0);
 
-while(check === 1){
-    var a = prompt('Enter first side length of a triangle: ', 0);
-
-    if (a <= 0){
-        alert('A triangle must have 3 sides with a positive definite length');
-    } else if (typeof a === 'undefined' || a === ''){
-        alert('Input values should be ONLY numbers');
-    }else{
-        break;
-    }
+if (a <= 0){
+    alert('A triangle must have 3 sides with a positive definite length');
+} else if (typeof a === 'undefined' || a === ''|| typeof a !== 'number'){
+    alert('Input values should be ONLY numbers');
 }
 
-while(check === 1){
-    var b = prompt('Enter second side length of a triangle: ', 0);
+let b = prompt('Enter second side length of a triangle: ', 0);
 
-    if (b <= 0){
-        alert('A triangle must have 3 sides with a positive definite length');
-    } else if (typeof b === 'undefined' || b === ''){
-        alert('Input values should be ONLY numbers');
-    }else{
-        break;
-    }
+if (b <= 0){
+    alert('A triangle must have 3 sides with a positive definite length');
+} else if (typeof b === 'undefined' || b === ''|| typeof b !== 'number'){
+    alert('Input values should be ONLY numbers');
 }
 
-while(check === 1){
-    var c = prompt('Enter third side length of a triangle: ', 0);
+let c = prompt('Enter third side length of a triangle: ', 0);
 
-    if (c <= 0){
-        alert('A triangle must have 3 sides with a positive definite length');
-    } else if (typeof c === 'undefined' || c === ''){
-        alert('Input values should be ONLY numbers');
-    }else{
-        break;
-    }
+if (c <= 0){
+    alert('A triangle must have 3 sides with a positive definite length');
+} else if (typeof c === 'undefined' || c === '' || typeof c !== 'number'){
+    alert('Input values should be ONLY numbers');
 }
 
 if (a+b>c && a+c>b && b+c>a){
-
     if (a === b && a === c){
         console.log('Equilateral triangle');
     } else if (a === b || a === c || b === c){
